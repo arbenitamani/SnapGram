fbccaa8f7e84ff33731786f1c3510f7d1108bc7a
Arbenita Mani <arbenita.mani123@gmail.com>
2024-01-21 02:25:44 +0100

    First-Commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './globals.css';
+import { Routes, Route } from 'react-router-dom';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages';
+import './globals.css';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+
+
+const App = () => {
+  return (
+   <main className='flex h-screen'>
+    <Routes>
+  
+    {/*Public Routes*/}
+
+    <Route element={<AuthLayout/>}> 
+    <Route path="/sign-in" element = {<SigninForm/>}/>
+    <Route path="/sign-up" element = {<SignupForm/>}/>
+   </Route>
+
+
+    {/*Private Routes*/}
+    <Route element={<RootLayout/>}> 
+    <Route index element = {<Home/>}/>
+    </Route>
+
+    </Routes>
+   </main>
+  )
+}
+
+export default App
